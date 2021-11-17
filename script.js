5b549fdaa5edcf23057bf99e40ab72741f8bac8f
Rahat Siddique <rahatsiddique2717@gmail.com>
2021-11-17 10:44:13 +0600

    after creating success message

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-function ticketItem(type,isIncrease){
+function ticketItem(type, isIncrease) {
     const ticketInput = document.getElementById(type + '-count');
     const ticketCount = parseInt(ticketInput.value);
     let ticketNewCount = ticketCount;
-    if(isIncrease == true){
+    if (isIncrease == true) {
         ticketNewCount = ticketCount + 1;
     };
-    if(isIncrease == false && ticketCount > 0){
+    if (isIncrease == false && ticketCount > 0) {
         ticketNewCount = ticketCount - 1;
     };
     ticketInput.value = ticketNewCount;
     calculatePrice();
 };
 
-function calculatePrice(){
+function calculatePrice() {
     const vipCount = getInputValue('vip');
     const economyCount = getInputValue('economy');
 
@@ -26,8 +26,31 @@ function calculatePrice(){
     document.getElementById('total-price').innerText = '$' + totalPrice;
 };
 
-function getInputValue(type){
+function getInputValue(type) {
     const typeInput = document.getElementById(type + '-count');
     const typeCount = parseInt(typeInput.value);
     return typeCount;
+}
+
+// Booking Successful/Unsuccessful Message :
+function showSuccessful() {
+    const bookingArea = document.getElementById('booking-area');
+    bookingArea.style.display = 'none';
+
+    const doneMessage = document.getElementById('done-message');
+    doneMessage.style.display = 'block';
+
+    setTicketsNumber();
+};
+
+function setTicketsNumber(){
+    let doneVip = document.getElementById('done-vip');
+    doneVip.innerText = getInputValue('vip');
+
+    let doneEconomy = document.getElementById('done-economy');
+    doneEconomy.innerText = getInputValue('economy');
+
+    const totalPrice = document.getElementById('total-price');
+    let doneTotal = document.getElementById('done-total');
+    doneTotal.innerText = totalPrice.innerText;
 }
